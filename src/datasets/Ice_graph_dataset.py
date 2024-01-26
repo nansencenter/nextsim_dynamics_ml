6efc9e8041d399422fef39633850d48fa19ce4f8
franamor98 <franamorroldan@gmail.com>
2024-01-26 10:43:40 +0100

    minor fix dataset

--- src/datasets/Ice_graph_dataset.py
+++ src/datasets/Ice_graph_dataset.py
@@ -1,19 +1,20 @@
-from torch_geometric.data import Dataset
-from torch_geometric.transforms import NormalizeFeatures
+from torch.utils.data import Dataset
 
+#Dataset: serves to fetch graph data on dataloaders or training loop. Its a standard torch class to manage information, transfrom and iterate over it
 class Ice_graph_dataset(Dataset):
-    def __init__(self,data_list):
+    def __init__(self, data_list, transform = None):
         super(Ice_graph_dataset, self).__init__()
 
-        # Define multiple instances of Data objects
+        # List of graphs
         self.data_list = data_list
-        self.transform = NormalizeFeatures(attrs=['x','edge_attr','y'])
+        self.transform = transform
 
-    def len(self):
+    def __len__(self):
         return len(self.data_list)
 
-    def get(self, idx):
-
-        data = self.transform(self.data_list[idx])
+    def __getitem__(self, idx):
+        data = self.data_list[idx]
+        if self.transform is not None:
+            data.x = self.transform(data.x.unsqueeze(dim=0).moveaxis(-1,0)).squeeze().moveaxis(0,-1)
 
         return data

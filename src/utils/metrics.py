7c201b5d72a5c6e154b0f455b558d0f3a73840e8
franamor98 <franamorroldan@gmail.com>
2024-02-19 19:06:52 +0100

    added func tot compute all metrics

--- src/utils/metrics.py
+++ src/utils/metrics.py
@@ -1,5 +1,70 @@
 import torch
+import torch.nn as nn
 import numpy as np
+
+
+
+def compute_metrics(
+        targets:torch.tensor,
+        predictions:torch.tensor,
+        init_coords:torch.tensor,
+        velocity:bool
+):
+    """ 
+    Compute metrics for the model
+    Arguments:
+        targets: torch.tensor
+            target values
+        predictions: torch.tensor
+            predicted values
+        init_coords: torch.tensor
+            initial coordinates
+        velocity: bool
+            if the model is predicting velocity or coordinates
+    Returns:
+        rmse_position: torch.tensor, 
+            rmse over position
+        mae_vel_norm: torch.tensor
+            mae over velocity norm
+        angles: torch.tensor
+            angle between predicted and target velocity
+    """
+    mse =  nn.MSELoss(reduction='none')
+    mae = nn.L1Loss(reduction='none')
+    if velocity:
+        #convert velocity to coordinates
+        target_vel = targets.reshape(2,-1).unsqueeze(1)
+        predicted_vel = predictions.reshape(2,-1).unsqueeze(1)
+        targets = targets + init_coords
+        predictions = predictions + init_coords
+    else:
+            targets =targets * 1000#to meters
+            predictions = predictions * 1000
+            
+    #get original coordinates in meters
+    target_coords = torch.stack([targets[:,:iterations],targets[:,iterations:]],dim=1)
+    predicted_coords = torch.stack([predictions[:,:iterations],predictions[:,iterations:]],dim=1)
+
+    #error over position
+    rmse_position = torch.sqrt(torch.mean(mse(target_coords,predicted_coords),dim=[1]))
+    #error over velocity
+    if not predict_vel:
+            target_coords = torch.stack([init_coords,target_coords.squeeze()],dim=-1)
+            predicted_coords = torch.stack([init_coords,predicted_coords.squeeze()],dim=-1)
+            target_vel = velocity_from_coords(np.array(target_coords))
+            predicted_vel = velocity_from_coords(np.array(predicted_coords))
+    #speed
+    target_vel_norm = velocity_norm(target_vel)
+    predicted_vel_norm =  velocity_norm(predicted_vel)
+    mae_vel_norm = mae(target_vel_norm,predicted_vel_norm)
+    #angle
+    angles = velocity_angle(predicted_vel,target_vel)
+
+    return rmse_position,mae_vel_norm,angles
+            
+
+
+
 def velocity_from_coords(coords:torch.tensor):
     """
     Function to compute velocity from coordinates
@@ -20,7 +85,7 @@ def velocity_from_coords(coords:torch.tensor):
         vs.append(v)
     vs = torch.tensor(vs)
     us = torch.tensor(us)
-    return torch.stack([us,vs])
+    return torch.stack([us,vs],dim=0)
 
 def velocity_norm(velocity: torch.tensor):
     """
@@ -34,7 +99,7 @@ def velocity_norm(velocity: torch.tensor):
             norm of shape (n_timesteps,samples)
 
     """
-    return torch.sqrt(velocity[0,:,:]**2 + velocity[1,:,:]**2)
+    return torch.sqrt(velocity[0]**2 + velocity[1]**2)
 
 def velocity_angle(pred_vel,target_vel: torch.tensor):
     """

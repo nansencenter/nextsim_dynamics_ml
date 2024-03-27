d88984d9f9c1d4b3b88e43c5e6919e8770a3a8c1
franamor98 <franamorroldan@gmail.com>
2024-03-27 10:02:02 +0100

    save/load files added to trainer

--- src/trainer.py
+++ src/trainer.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 from torch.optim.lr_scheduler import ReduceLROnPlateau, ExponentialLR
 from torch_geometric.loader import DataLoader
+import shutil
 
 
 from models.GNNs import GCNN_2G
@@ -10,7 +11,7 @@ from models.training_utils import process_dataloader
 from datasets.Ice_graph_dataset import Ice_graph_dataset
 from ice_graph.ice_graph import Ice_graph
 from utils.graph_utils import compute_normalization_batch
-
+from utils.metrics import CustomIceLoos
 
 import numpy as np
 from tqdm import tqdm
@@ -78,37 +79,66 @@ def main(
     radius = 500000 #meters
     iterations = 1
     time_index = 3 #index of element graph to fetch samples from
-    time_index_val = 20
-
-    samples_train = nextsim.get_samples_area((0,0),radius,time_index=time_index,n_samples=n_generations,elements=False)
-    samples_val = nextsim.get_samples_area((0,0),radius,time_index=time_index_val,n_samples=int(n_generations/5),elements=False)
+    time_index_val = 12
 
 
-    train_graph_list = []
-    fet = ['Damage', 'Concentration', 'Thickness', 'M_wind_x', 'M_wind_y', 'M_ocean_x', 'M_ocean_y', 'x', 'y']
 
-    train_graph_list = nextsim.get_samples_graph(
-        (0,0),
-        radius,
-        [time_index,time_index+2,time_index+4,time_index+6,time_index+8,time_index+10,time_index+12,time_index+14,time_index+16],
-        n_samples=n_generations,
-        target_iter=iterations,
-        e_features=fet,
-        include_vertex=True,
-        pred_velocity=predict_vel
+    remove_previous = False
+    nextsim.step = 1
+    neighbours = 3
+    training_center = (0,0)
+    validation_center = (0,0)
 
-    )
-    val_graph_list = nextsim.get_samples_graph(
-        (0,0),
-        radius,
-        [time_index_val,time_index_val+2,time_index_val+4],
-        n_samples=int(n_generations/5),
-        target_iter=iterations,
-        e_features=fet,
-        include_vertex=True,
-        pred_velocity=predict_vel
+    save_dir = f'../data_graphs/{n_generations}_{int(radius/1000)}km_{time_index}tt_{time_index_val}tv_{neighbours}neigh_{nextsim.step}_step'
 
-    )
+    if remove_previous:
+        if os.path.exists(save_dir):
+            #remove dir and contents
+            shutil.rmtree(save_dir)
+
+
+    if not os.path.exists(save_dir):
+        
+
+        train_graph_list = []
+        fet = [ 'Concentration', 'Thickness', 'x', 'y']
+        print("### Getting training samples ###\n")
+        train_graph_list = nextsim.get_samples_graph(
+            training_center,
+            radius,
+            [time_index,time_index+2,time_index+4,time_index+6,time_index+8,time_index+10],
+            n_samples=n_generations,
+            target_iter=iterations,
+            e_features=fet,
+            include_vertex=True,
+            pred_velocity=predict_vel,
+            n_neighbours=neighbours
+        )
+
+        print("### Getting validation samples ###\n")
+        val_graph_list = nextsim.get_samples_graph(
+            validation_center,
+            radius,
+            [time_index_val,time_index_val+2,time_index_val+4,time_index_val+6],
+            n_samples=int(n_generations/5),
+            target_iter=iterations,
+            e_features=fet,
+            include_vertex=True,
+            pred_velocity=predict_vel,
+            n_neighbours=neighbours
+        )
+
+
+        print("### saving data ###\n")
+        os.makedirs(save_dir, exist_ok=True)
+        # Save training and validation graphs
+        torch.save(train_graph_list, os.path.join(save_dir, 'training.pt'))
+        torch.save(val_graph_list, os.path.join(save_dir, 'validation.pt'))
+
+    else:
+        print("Data already exists")
+        train_graph_list = torch.load(os.path.join(save_dir, 'training.pt'))
+        val_graph_list = torch.load(os.path.join(save_dir, 'validation.pt'))
 
     #compute norm transform
     transform_train = compute_normalization_batch(train_graph_list)
@@ -116,6 +146,7 @@ def main(
 
     #create datasets and loaders
     batch_size = 128
+    print(f"Batch size: {batch_size}")
     train_dataset = Ice_graph_dataset(train_graph_list, transform=None)
     val_dataset = Ice_graph_dataset(val_graph_list, transform=None)
     train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
@@ -123,19 +154,19 @@ def main(
 
     e_graph = next(iter(train_dataset))[0] #just to get the num_features
     num_features1 = e_graph.x.shape[-1]  # Node feature dimension
-
     v_graph = next(iter(train_dataset))[1] #just to get the num_features
     num_features2 = v_graph.x.shape[-1]  # Node feature dimension
 
-    hidden_channels = 12
+    hidden_channels = ([num_features1*2,num_features1*3,20],[num_features2*2,num_features2*3,20])
     num_classes = e_graph.y[0].shape[0] # trajectory lenght *2, since we have x,y.
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = GCNN_2G(num_features1,num_features2, hidden_channels, num_classes,dropout=0.2).to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.02, weight_decay=5e-4)
-    scheduler = ExponentialLR(optimizer, gamma=0.95)
-    loss = nn.L1Loss()
-    num_epochs = 100
+    model = GCNN_2G(num_features1,num_features2, hidden_channels, num_classes,dropout=0.1).to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.005, weight_decay=7e-4)
+    scheduler = ExponentialLR(optimizer, gamma=0.9)
+    #loss = nn.L1Loss()
+    loss = CustomIceLoos(A=1,B=0,C=0,step=nextsim.step,d_time=nextsim.d_time)
+    num_epochs = 10
 
     # Define the number of epochs
     train_model(model, optimizer, scheduler, num_epochs, device, loss, train_dataloader,val_dataloader)
